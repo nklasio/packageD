b7013d9349326df08c8e73b8ae231c2c583128da
Niklas Stambor <n.stambor@gmx.de>
2018-01-31 23:41:06 +0100

    Added EnvironmentManager Added Logger Added Filestructure

--- source/app.d
+++ source/app.d
@@ -1,6 +1,44 @@
 import std.stdio;
+import std.getopt;
 
-void main()
+
+int main(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+	import EnvironmentManager : EnvironmentManager;
+	EnvironmentManager.initialize();
+
+	string pack = "";
+	bool handlerFailed = false;
+	if(args.length == 1) {
+		ver();
+	}
+
+	GetoptResult helpInformation = getopt(args, "version|v", "Show version of packaged", &ver,
+									 			"environment|env", "Debug Environment Variables", &eDebug);
+
+	if(helpInformation.helpWanted) {
+		defaultGetoptPrinter("usage: packaged <operation> [...]", helpInformation.options);
+	} 
+
+	
+
+	return handlerFailed ? 1 : 0;
 }
+
+private:
+static void ver() {
+	printf("\n");
+		printf("packageD - © by Niklas Stambor\n");
+		printf("A Dlang based package manager\n");
+		printf("This program may be freely redistributed\nunder the terms of the GNU General Public License V3.\n");
+		printf("\n");
+		printf("usage: packaged <operation> [...]\n");
+		printf("Use --help or \"man packaged\" to get help\n");
+		printf("\n");
+}
+
+static void eDebug() {
+	import EnvironmentManager : EnvironmentManager;
+	EnvironmentManager.dDebug();
+}
+
