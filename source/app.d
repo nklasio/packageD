e2163a7a3dcc0b4883805814179c6e5e72c8b60b
Niklas Stambor <n.stambor@gmx.de>
2018-02-01 11:40:39 +0100

    Encapsuling app.d

--- source/app.d
+++ source/app.d
@@ -1,36 +1,6 @@
-import std.stdio;
-import std.getopt;
-
-
-int main(string[] args)
+void main(string[] args)
 {
-	import core.EnvironmentManager : EnvironmentManager;
-	EnvironmentManager.initialize();
-
-	string pack = "";
-	bool handlerFailed = false;
-	if(args.length == 1) {
-		ver();
-	}
-
-	GetoptResult helpInformation = getopt(args, "version|v", "Show version of packaged", &ver);
-
-	if(helpInformation.helpWanted) {
-		defaultGetoptPrinter("usage: packaged <operation> [...]", helpInformation.options);
-	} 
-
-
-	return handlerFailed ? 1 : 0;
+	import core.packageD;
+	auto packD = new packageD(args);
 }
 
-private:
-static void ver() {
-	printf("\n");
-		printf("packageD - © by Niklas Stambor\n");
-		printf("A Dlang based package manager\n");
-		printf("This program may be freely redistributed\nunder the terms of the GNU General Public License V3.\n");
-		printf("\n");
-		printf("usage: packaged <operation> [...]\n");
-		printf("Use --help or \"man packaged\" to get help\n");
-		printf("\n");
-}
---
+++ source/core/packageD.d
@@ -0,0 +1,35 @@
+module core.packageD;
+
+class packageD {
+    import core.EnvironmentManager : EnvironmentManager;
+    import std.stdio : printf;
+    import std.getopt;
+
+    this(string[] args) {
+        if(args.length == 1)
+            ver();
+	    EnvironmentManager.initialize();
+        EnvironmentManager.sharedLogger.log("[I]Initializing packageD");
+        auto helpInformation = getopt(args, "version|v", "Show version of packaged", &ver);
+
+        if(helpInformation.helpWanted) {
+            defaultGetoptPrinter("usage: packaged <operation> [...]", helpInformation.options);
+        } 
+    }
+
+    public static void ver() {
+	    printf("\n");
+		printf("packageD - © by Niklas Stambor\n");
+		printf("A Dlang based package manager\n");
+		printf("This program may be freely redistributed\nunder the terms of the GNU General Public License V3.\n");
+		printf("\n");
+		printf("usage: packaged <operation> [...]\n");
+		printf("Use --help or \"man packaged\" to get help\n");
+		printf("\n");
+    }
+
+    ~this() {
+        EnvironmentManager.sharedLogger.log("[I]Destructing packageD");
+    }
+
+}
