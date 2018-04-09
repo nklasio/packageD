e2c9e6979dc9747e4ad92297695984925d589672
Niklas Stambor <n.stambor@gmx.de>
2018-04-09 13:59:59 +0200

    Fixed Imports and removed support for AUR packages

--- source/core/RepositoryManager.d
+++ source/core/RepositoryManager.d
@@ -1,154 +1,27 @@
 module core.RepositoryManager;
 
-enum RepositoryType {
-    AUR = "aur",
-    pacD = "pacD"
-}
 import std.json;
 import core.EnvironmentManager;
 import std.stdio : writeln;
 
+import std.stdio;
+import std.string;
+import std.algorithm.iteration;
+import std.algorithm.searching;
+
+import std.array;
+
+import std.net.curl;
+import std.json;
+import std.file;
+
+import std.process;
+import std.conv : to;
+
 
 class RepositoryManager {
     public:
-    void Info(RepositoryType type, JSONValue pac) {
-        import std.string : format;
-
-        writeln(format("Found 1 package! %s", pac["Name"].str));
-        writeln(format("Description: %s", pac["Description"].str));
-        import std.array : array, join;
-        string[] dependencies;
-        if("Depends" in pac) {
-            auto s = pac["Depends"].array;
-            foreach(p; s) {
-                dependencies ~= p.str;
-            }
-            writeln(format("Requiered dependencies[%s]: %s", dependencies.length, join(dependencies, ", ")));
-        }
-        bool failed = false;
-        foreach(d; dependencies) {
-            if(!Request(type, d, true)) {
-                writeln(format("Abort! Could not resolve dependencies for %s! \nUnresolvable dependency: %s", pac["Name"].str, d));
-                failed = true;
-                break;
-            }
-        }
-        if (failed)
-            return;
-        
-        if(true) {
-
-        }
-        //TODO: CHECK FOR USER
-        GatheringPackage(type, pac);
-        foreach(d; dependencies) { 
-            GatheringPackage(type, pac);
-        }
-
-    }
-
-    bool GatheringPackage(RepositoryType type, JSONValue pac) {
-        import std.net.curl : download, CurlException;
-        import std.stdio : writeln;
-        import std.string : format;
-        import std.algorithm.iteration : filter;
-        import std.algorithm.searching : canFind;
-        
-        import std.array: array;
-
-
-        final switch(type) {
-            case RepositoryType.AUR: 
-                auto p = filter!(rep => canFind(rep, "aur"))(mirrors);
-                import std.file : exists, getSize;
-                foreach(r; p) {
-                    try {
-                        import cache.PackageCache : PackageCache;
-                        string pacFile = format("%s%s.tar.gz", EnvironmentManager.setupSubFolder(EnvironmentManager.repositoryDirectory, "aur"), pac["Name"].str);
-                        if(PackageCache.cacheContains(type, pac["Name"].str)) {
-                            writeln(format("Package already in cache. Skipping download! | %s", getSize(pacFile)));
-                            return true;
-                        }
-                        writeln("Downloading package...");
-                        download(format("%s%s",r, pac["URLPath"].str), pacFile);
-                        if(exists(pacFile)) {
-                            writeln(format("Successfully downloaded! %s.tar.gz | %s", pac["Name"].str, getSize(pacFile)));
-                            PackageCache.addToCache(RepositoryType.AUR, pac["Name"].str);
-                            return true;
-                        }
-                    }
-                    catch(CurlException exc) {
-                        writeln(format("%s | %s", exc.message, r));
-                        EnvironmentManager.sharedLogger.error(format("%s | %s", exc.message, r));
-                    }
-                }
-
-                break;
-            case RepositoryType.pacD: 
-                break;
-        }
-
-        //cgit/aur.git/snapshot/spotify.tar.gz"
-        return false;
-    }
-
-    bool Request(RepositoryType RepositoryType, string pac, bool dependency = false) {
-        import std.stdio : writeln;
-        import std.string : format;
-        import std.algorithm.iteration : filter;
-        import std.algorithm.searching : canFind;
-        
-        import std.array: array;
-        
-        import std.net.curl : get, CurlException;
-        import std.json : parseJSON, JSONValue;
-
-        JSONValue packet = null; 
-        
-
-        final switch(RepositoryType) {
-            case RepositoryType.AUR: 
-                auto p = filter!(rep => canFind(rep, "aur"))(mirrors);
-
-                foreach(r; p) {
-                    if(!packet.isNull)
-                        break;
-
-                    try {
-                        auto request = parseJSON(get(format("%srpc/?v=5&type=info&arg[]=%s", r, pac))).object;
-                        if(request["resultcount"].integer > 0){
-                            foreach(result; request["results"].array) {
-                                if(result["Name"].str == pac) {
-                                    packet = result;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    catch(CurlException exc) {
-                        if(!dependency) writeln(format("%s | %s", exc.message, r));
-                        EnvironmentManager.sharedLogger.error(format("%s | %s", exc.message, r));
-                    }
-                }
-                break;
-            case RepositoryType.pacD:
-                    break;
-        }
-        import std.file : write;
-        if(!packet.isNull) {
-            write("debug.request", packet.toString());
-            Info(RepositoryType, packet);
-            return true;
-        } else {
-            if(!dependency) writeln(format("Could not find package: %s", pac));
-            return false;
-        }
-    }
-
-    bool RequestPacD(JSONValue pacDObject) {
-        import std.string;
-        import std.file;
-
+        bool RequestPacD(JSONValue pacDObject) {
         //writeln(pacDObject["package"]);
         auto pack = pacDObject["package"];
         writeln("_____________________________________________");
@@ -158,17 +31,12 @@ class RepositoryManager {
         writeln(format("Author: %s", pack["author"].str));
         writeln("_____________________________________________");
         writeln(format("You are about to install \"%s\" : v.%s by %s. Continue installing? [Y|n]", pack["name"].str, ver, pack["author"].str));
-        import std.stdio : stdin;
-        import std.algorithm.searching : canFind;
-        auto read = stdin.readln();
+              auto read = stdin.readln();
         if(read.canFind("n")){
             writeln("Installation abort by user!");
             return false;
         } else {
-            import std.process;
-            import std.file : chdir;
-            import std.array : replace, split;
-            import std.conv : to;
+
 
             import core.EnvironmentManager : EnvironmentManager;
             writeln("Installing...");
@@ -257,13 +125,6 @@ class RepositoryManager {
         this.mirrors ~= mirrors;
     }
 
-    this() {
-        this.mirrors = [];
-    }
-
-    ~this() {
-        
-    }
     private:
-    string[] mirrors;
+    string[] mirrors = [];
 }
